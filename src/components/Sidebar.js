576d4bad0a6575c16639516af88eb7f4813640fb
Jose Sarmiento <jose.dev.official@gmail.com>
2022-03-11 07:08:49 +0800

    add searchbox in header and sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,63 +1,75 @@
-import React from 'react'
-import {NavLink, Link} from "react-router-dom"
-import useAppContext from "../hooks/useAppContext"
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import Search from "./Search"
+import useAppContext from "../hooks/useAppContext";
 
 const Sidebar = () => {
-	const {isOpenSidebar, closeSidebar} = useAppContext()
+	const { isOpenSidebar, closeSidebar } = useAppContext();
 
 	return (
 		<div className={isOpenSidebar ? "sidebar sidebar--show" : "sidebar"}>
-			<button className="sidebar__close" onClick={() => closeSidebar()}>Close</button>
+			<button className="sidebar__close" onClick={() => closeSidebar()}>
+				Close
+			</button>
+			<Search />
 			<ul className="sidebar__links">
-					<li className="sidebar__item">
-						<NavLink
-							to="/"
-							className={({ isActive }) =>
-								isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"
-							}
-						>
-							<i className="fa-solid fa-house"></i>
-							Home
-						</NavLink>
-					</li>
+				<li className="sidebar__item">
+					<NavLink
+						to="/"
+						className={({ isActive }) =>
+							isActive
+								? "sidebar__link sidebar__link--active"
+								: "sidebar__link"
+						}
+					>
+						<i className="fa-solid fa-house"></i>
+						Home
+					</NavLink>
+				</li>
 
-					<li className="sidebar__item">
-						<NavLink
-							to="/p/hotels"
-							className={({ isActive }) =>
-								isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"
-							}
-						>
-							<i className="fa-solid fa-shoe-prints"></i>
-							Places to Stay
-						</NavLink>
-					</li>
+				<li className="sidebar__item">
+					<NavLink
+						to="/p/hotels"
+						className={({ isActive }) =>
+							isActive
+								? "sidebar__link sidebar__link--active"
+								: "sidebar__link"
+						}
+					>
+						<i className="fa-solid fa-shoe-prints"></i>
+						Destinations
+					</NavLink>
+				</li>
 
-					<li className="sidebar__item">
-						<NavLink
-							to="/host/guide"
-							className={({ isActive }) =>
-								isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"
-							}
-						>
-							<i className="fa-solid fa-user-plus"></i>
-							Become a Host
-						</NavLink>
-					</li>
+				<li className="sidebar__item">
+					<NavLink
+						to="/host/guide"
+						className={({ isActive }) =>
+							isActive
+								? "sidebar__link sidebar__link--active"
+								: "sidebar__link"
+						}
+					>
+						<i className="fa-solid fa-user-plus"></i>
+						Become a Host
+					</NavLink>
+				</li>
 
-					<li className="sidebar__item">
-						<NavLink
-							to="/account/profile"
-							className={({ isActive }) =>
-								isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"
-							}
-						>
-							<i className="fa-solid fa-user"></i>
-							My Account
-						</NavLink>
-					</li>
-				</ul>
-			
+				<li className="sidebar__item">
+					<NavLink
+						to="/account/profile"
+						className={({ isActive }) =>
+							isActive
+								? "sidebar__link sidebar__link--active"
+								: "sidebar__link"
+						}
+					>
+						<i className="fa-solid fa-user"></i>
+						My Account
+					</NavLink>
+				</li>
+			</ul>
+
 			<div className="sidebar__item">
 				<Link to="#" className="sidebar__link">
 					<i className="fa-solid fa-gear sidebar__cog"></i>
@@ -65,7 +77,7 @@ const Sidebar = () => {
 				</Link>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Sidebar
+export default Sidebar;
